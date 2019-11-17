f5413aca5422b21d9fa094fbf9a96f9474487e35
Jean-Marc Wagner <jeanmarc.wagner@gmx.de>
2019-11-17 18:22:39 +0100

    added timetrack resource

--- app/api/timetrack/resource.py
+++ app/api/timetrack/resource.py
@@ -1,2 +1,20 @@
 from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
+from datetime import datetime
+
+from app.api.timetrack.models import WorkDay, WorkDaySchema
+from app.database import db
+
+class TimeListApi(Resource):
+    @jwt_required
+    def get(self):
+        pass
+
+class TimeApi(Resource):
+    @jwt_required
+    def post(self):
+        pass
+    
+    def put(self):
+        pass
